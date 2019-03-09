75e55cc774adbda0314c630cb8cfeedb7ae50fe6
vladuma <45532103+vladuma@users.noreply.github.com>
2019-03-09 09:43:51 +0200

    Updates

---
+++ precache-manifest.48b17a8297024bc782617e69ef9674f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbaa45638be0838624a4",
+    "url": "/react_app/static/css/main.3dfe9fa1.chunk.css"
+  },
+  {
+    "revision": "dbaa45638be0838624a4",
+    "url": "/react_app/static/js/main.dc96ba6f.chunk.js"
+  },
+  {
+    "revision": "10c67c8a2e7bca023720",
+    "url": "/react_app/static/js/runtime~main.749f4b56.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/react_app/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3308a4c1f39baf344b88f2e400f26e0b",
+    "url": "/react_app/index.html"
+  }
+];
